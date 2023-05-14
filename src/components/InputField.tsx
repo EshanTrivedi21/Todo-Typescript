8b4c94fcdd371151ce0f267a1f7a028e51b53f39
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-05-14 22:14:17 +0530

    feat: input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './styles/InputField.scss';
 
 const InputField: React.FC = () => {
-	return <div>InputField</div>;
+	return (
+		<>
+			<form className='input'>
+				<input
+					type='input'
+					placeholder='Please Enter a Task'
+					className='input__box'
+				/>
+				<button
+					type='submit'
+					className='input__btn'
+				>
+					<i className='fa-solid fa-plus input__btn__icon'></i>
+				</button>
+			</form>
+		</>
+	);
 };
 
 export default InputField;
